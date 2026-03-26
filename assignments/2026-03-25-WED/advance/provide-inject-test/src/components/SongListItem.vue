3b4a6f5ad30df45699f5452c10f8d61b18a21207
jinwoo <jjw6654@gmail.com>
2026-03-26 10:30:20 +0900

    solve: 컴포넌트 심화(심화) 문제 풀이

---
+++ assignments/2026-03-25-WED/advance/provide-inject-test/src/components/SongListItem.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'SongListItem',
+  inject: ['icons'],
+  props: ['song'],
+};
+</script>
+
+<template>
+  <li>
+    <i :class="song.done ? icons.checked : icons.unchecked"></i>
+    {{ song.title }}
+  </li>
+</template>
---
+++ assignments/2026-03-25-WED/advance/v-model-test/src/components/InputName.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  props: ['name'],
+};
+</script>
+
+<template>
+  <input
+    type="text"
+    :value="name"
+    @input="$emit('update:name', $event.target.value)"
+  />
+</template>
