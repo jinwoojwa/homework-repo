91c03bc742cc6c128022a448219bbf79240af273
jinwoo <jjw6654@gmail.com>
2026-03-26 09:35:04 +0900

    solve: 컴포넌트 심화(기본) 문제 풀이

---
+++ assignments/2026-03-25-WED/basic/style-test/src/components/Child3.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: 'Child3',
+  created() {
+    console.log(this.$style);
+  },
+};
+</script>
+
+<template>
+  <div class="$style.child">
+    <h2>Child3</h2>
+  </div>
+</template>
+
+<style module>
+.child {
+  background-color: orange;
+  border: solid 1px black;
+  margin: 1.5em;
+  padding: 1em;
+}
+</style>
